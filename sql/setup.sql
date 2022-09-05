3dbdf148d9f37b0d1445296a9029db0e0eeeaa4e
hulkmtz <lmartinezcorral0312@gmail.com>
2022-09-04 19:37:52 -0600

    creada base de datos .sql

---
+++ sql/setup.sql
@@ -0,0 +1,12 @@
+
+CREATE DATABASE contacts_app;
+
+USE contacts_app;
+
+CREATE TABLE contacts(
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    name VARCHAR(255),
+    phone_number VARCHAR(255)
+
+);
+INSERT INTO contacts(name, phone_number) VALUES ("Pepe", "6251000101");
